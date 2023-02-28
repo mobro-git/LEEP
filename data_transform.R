ac77bfb3b501332ea6a8a2bf0ecbdd98d5b49c60
Cole <Cole.Jefferson@epa.gov>
2023-02-28 10:11:09 -0500

    unit conversions

--- data_transform.R
+++ data_transform.R
@@ -4,39 +4,55 @@ library(tidyverse)
 library(readxl)
 library(xlsx)
 
-data.emissions <- read_xlsx("data-raw/model-runs/ira_comparison.xlsx", sheet = "Emissions") %>%
+data.emissions <- read_xlsx("data-extra/ira_comparison_raw/ira_comparison.xlsx", sheet = "Emissions") %>%
   gather("year", "value", 4:9) %>%
   mutate(table = "emissions")
 
-data.co2captured <- read_xlsx("data-raw/model-runs/ira_comparison.xlsx", sheet = "CO2 Captured") %>%
+data.co2captured <- read_xlsx("data-extra/ira_comparison_raw/ira_comparison.xlsx", sheet = "CO2 Captured") %>%
   gather("year", "value", 4:9) %>%
   mutate(table = "co2_captured")
 
-data.costs <- read_xlsx("data-raw/model-runs/ira_comparison.xlsx", sheet = "Costs") %>%
-  gather("year", "value", 4:9) %>%
-  mutate(table = "costs")
+# data.costs <- read_xlsx("data-extra/ira_comparison_raw/ira_comparison.xlsx", sheet = "Costs") %>%
+#   gather("year", "value", 4:9) %>%
+#   mutate(table = "costs")
 
-data.elecdemand <- read_xlsx("data-raw/model-runs/ira_comparison.xlsx", sheet = "Electricity Demand") %>%
+data.elecdemand <- read_xlsx("data-extra/ira_comparison_raw/ira_comparison.xlsx", sheet = "Electricity Demand") %>%
   gather("year", "value", 4:9) %>%
-  mutate(table = "elec_demand")
+  mutate(table = "elec_demand") %>%
+  mutate(cal_value = value*0.0036) %>%
+  # converting from TWh to EJ/Yr
+  select(-value) %>%
+  rename(value = cal_value) %>%
+  mutate(
+    unit = case_when(
+      unit == "TWh" ~ "EJ/yr",
+      TRUE ~ "unit"))
 
-data.nox <- read_xlsx("data-raw/model-runs/ira_comparison.xlsx", sheet = "NOx") %>%
-  gather("year", "value", 4:9) %>%
-  mutate(table = "nox")
+# data.nox <- read_xlsx("data-extra/ira_comparison_raw/ira_comparison.xlsx", sheet = "NOx") %>%
+#   gather("year", "value", 4:9) %>%
+#   mutate(table = "nox")
+#
+# data.so2 <- read_xlsx("data-extra/ira_comparison_raw/ira_comparison.xlsx", sheet = "SO2") %>%
+#   gather("year", "value", 4:9) %>%
+#   mutate(table = "so2")
 
-data.so2 <- read_xlsx("data-raw/model-runs/ira_comparison.xlsx", sheet = "SO2") %>%
-  gather("year", "value", 4:9) %>%
-  mutate(table = "so2")
-
-data.ffconsump <- read_xlsx("data-raw/model-runs/ira_comparison.xlsx", sheet = "Fossil Fuel Consumption") %>%
+data.ffconsump <- read_xlsx("data-extra/ira_comparison_raw/ira_comparison.xlsx", sheet = "Fossil Fuel Consumption") %>%
   gather("year", "value", 4:10) %>%
-  mutate(table = "ff_consump")
+  mutate(table = "ff_consump") %>%
+  mutate(cal_value = value*1.05505585262) %>%
+  # converting from quads to EJ/Yr
+  select(-value) %>%
+  rename(value = cal_value) %>%
+  mutate(
+    unit = case_when(
+      unit == "quads" ~ "EJ/yr",
+      TRUE ~ "unit"))
 
 combined.data <- full_join(data.emissions, data.co2captured) %>%
-  full_join(data.costs) %>%
+  # full_join(data.costs) %>%
   full_join(data.elecdemand) %>%
-  full_join(data.nox) %>%
-  full_join(data.so2) %>%
+  # full_join(data.nox) %>%
+  # full_join(data.so2) %>%
   full_join(data.ffconsump)
   #write.csv("bistline_ira.csv")
 
@@ -100,7 +116,7 @@ transformed.data <- combined.data %>%
   filter(variable != "delete") %>%
   select(-table)
 
-write.csv(transformed.data, "bistline_ira_tall.csv", row.names = FALSE)
+write.csv(transformed.data, "data-raw/model-runs/bistline_ira_tall.csv", row.names = FALSE)
 
 
 ##### variable renaming to EMF37 standard ######
